bf727077fa8efc29aeca35b88d7428f86fdf63e7
YucanLi <1579193739@qq.com>
2023-12-22 10:55:20 -0500

    Changes to be committed: 	modified:   404.html 	modified:   config/allowedOrigins.js 	modified:   index.html

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,11 +1,10 @@
 const allowedOrigins = [
-    'https://www.yoursite.com',
     'http://127.0.0.1:5500',
     'http://localhost:3500',
     'http://localhost:3000',
-    'http://localhost:3001',
     'https://master.dbs29o8fuwcy2.amplifyapp.com/',
-    'https://65854511b418e40008300307--bucolic-druid-71c9aa.netlify.app'
+    'https://65854511b418e40008300307--bucolic-druid-71c9aa.netlify.app',
+    'https://master--bucolic-druid-71c9aa.netlify.app/'
 ];
 
 module.exports = allowedOrigins;
